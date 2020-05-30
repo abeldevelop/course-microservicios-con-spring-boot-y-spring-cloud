8542ca72ba895a4c56e367f4beeacefe225083cf
Abel <abeldevelop@users.noreply.github.com>
2020-05-30 16:14:02 +0200

    Seccion 2 - Microservicios: la base - Video 011 - Creando el controlador rest productos

---
+++ servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
@@ -0,0 +1,30 @@
+package com.formacionbdi.springboot.app.productos.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.service.ProductoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class ProductoController {
+
+	private final ProductoService productoService;
+	
+	@GetMapping("/listar")
+	public List<Producto> findAllProducts() {
+		return productoService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}")
+	public Producto findProductoById(@PathVariable Long id) {
+		return productoService.findById(id);
+	}
+	
+}
