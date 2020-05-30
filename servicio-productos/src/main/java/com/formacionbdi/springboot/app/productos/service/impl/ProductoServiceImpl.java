bf57674f310ec8781d5e58acea92c05a1de54e00
Abel <abeldevelop@users.noreply.github.com>
2020-05-30 16:07:57 +0200

    Seccion 2 - Microservicios: la base - Video 010 - Creando el componente service para los productos

---
+++ servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/service/impl/ProductoServiceImpl.java
@@ -0,0 +1,32 @@
+package com.formacionbdi.springboot.app.productos.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.models.repository.ProductoRepository;
+import com.formacionbdi.springboot.app.productos.service.ProductoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	private final ProductoRepository repository;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Producto> findAll() {
+		return repository.findAll();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Producto findById(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+}
