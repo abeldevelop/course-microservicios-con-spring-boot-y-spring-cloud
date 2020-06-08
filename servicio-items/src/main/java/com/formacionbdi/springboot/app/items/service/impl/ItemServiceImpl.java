e962546e6fa350f7f545b229acbe5f812ad55167
Abel <abeldevelop@users.noreply.github.com>
2020-06-08 12:48:59 +0200

    Seccion 2 - Microservicios: la base - Video 020 - Utilizando Ribbon para balanceo de carga en RestTemplate

--- servicio-items/src/main/java/com/formacionbdi/springboot/app/items/service/impl/ItemServiceImpl.java
+++ servicio-items/src/main/java/com/formacionbdi/springboot/app/items/service/impl/ItemServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,7 @@ import com.formacionbdi.springboot.app.items.service.ItemService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Primary
 @Service
 public class ItemServiceImpl implements ItemService {
 
@@ -23,14 +25,14 @@ public class ItemServiceImpl implements ItemService {
 	
 	
 	public List<Item> findAll() {
-		List<Producto> productos = Arrays.asList(restTemplate.getForObject("http://localhost:8001/listar", Producto[].class));
+		List<Producto> productos = Arrays.asList(restTemplate.getForObject("http://servicio-productos/listar", Producto[].class));
 		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 	
 	public Item findById(Long id, Integer cantidad) {
 		Map<String, String> pathParams = new HashMap<>();
 		pathParams.put("id", id.toString());
-		Producto producto = restTemplate.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathParams);
+		Producto producto = restTemplate.getForObject("http://servicio-productos/ver/{id}", Producto.class, pathParams);
 		return new Item(producto, cantidad);
 	}
 	
